afb2aef9bce6c216246d97a50a7a4bae18cdfee5
drshny03 <darshanyadav7049@gmail.com>
2025-02-18 23:06:21 +0530

    [Darshan Yadav] Add Java code to filter records from a CSV file with CSV file in resources

---
+++ src/main/java/com/tit/day01/intermediate_problems/FilterRecords.java
@@ -0,0 +1,44 @@
+package com.tit.day01.intermediate_problems;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static java.lang.System.out;
+
+public class FilterRecords {
+
+    public static void main(String ar[]) throws IOException {
+        String filePath = "C:\\capgeminitraining\\Week-5\\CSV-Data-Handling\\src\\main\\resources\\student.csv";
+
+        FileReader reader = new FileReader(filePath);
+
+        try(BufferedReader bufferedReader = new BufferedReader(reader))
+        {
+            String line;
+           boolean header = true;
+
+            while((line=bufferedReader.readLine())!=null)
+            {
+                String [] columns = line.split(",");
+
+                if(header)
+                {
+                    header = false;
+                    continue;
+                }
+                if(Integer.parseInt(columns[3])>80)
+                {
+                    out.println("ID: " + columns[0] + ", Name: " + columns[1]+", Age: " + columns[2] + ", Marks: " + columns[3]);
+
+                }
+
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
